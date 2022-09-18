65ba2b3d4b4b9db5192d03a3834e3d2023aa4793
Keneshin <kuraysovyy@vk.com>
2022-09-18 21:19:42 +0600

    fix fucky wucky

--- src/dispatch/messageCreate.js
+++ src/dispatch/messageCreate.js
@@ -4,7 +4,7 @@ module.exports = {
     data.sendMessage = async (content, options = {}) => {
       if (typeof content == "object") options = content
       if (typeof content == "string") options.content = content
-      return await client.api.sendMessage(message.channel.id, options)
+      return await client.api.sendMessage(data.channel.id, options)
     }
     client.emit("message", data)
     client.emit("messageCreate", data)
